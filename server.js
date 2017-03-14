d3b32a54442fbed5e9c5df0b786203a628fbea62
RizkiDPrast <rizkidwip@gmail.com>
2017-03-14 13:34:50 +0700

    Rizki api merge

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+const express = require('express')
+const app = express()
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+app.use(bodyParser.urlencoded({extended: true}))
+
+// app.use(express.static('public'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  // if (mongoURL == null) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
       mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
+      // var mongoHost =  '127.0.0.1',
+      //     mongoPort =  27017,
+      //     mongoDatabase = 'user',
+      //     mongoUser, mongoPassword;
+
 
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = 'mongodb://';
@@ -31,21 +39,21 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     // Provide UI label that excludes user id and pw
     mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
   }
 }
+
 var db = null,
     dbDetails = new Object();
+
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
 
   var mongodb = require('mongodb');
   if (mongodb == null) return;
-
   mongodb.connect(mongoURL, function(err, conn) {
     if (err) {
-      callback(err);
+      console.log(err);
       return;
     }
 
@@ -59,35 +67,54 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
+  //res.send('used for api only')
+  res.sendFile("./index.html", {root:__dirname})
+});
+
+app.get('/guestlist', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
     initDb(function(err){});
   }
   if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
+    var g = db.collection('guest');
+        g.find().toArray(function(err, doc) {
+          if (err) {
+            res.status(500).send('Ops did not work');
+          }
+          console.log(doc)
+          res.send('myguest:' + JSON.stringify(doc));
+        })
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.send('none');
   }
 });
 
-app.get('/pagecount', function (req, res) {
+app.post('/sendguest', function (req, res) {
+  var name = req.body.name || "unknown",
+      phone = req.body.phone || '(empty)',
+      comment = req.body.comment || 'none';
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function(err){console.log('error connectiong to mongodb ' + err)});
   }
   if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
+    var g = db.collection('guest');
+        g.insert({
+          name: name,
+          phone: phone,
+          comment: comment
+        }, function(err, result) {
+          console.log('result:' + result);
+          g.find({name:name}).toArray(function(err, r){
+            console.log('r:' + r);
+          })
+          res.status(200).jsonp({"success": "true"});
+        })
   } else {
-    res.send('{ pageCount: -1 }');
+    res.status(400).jsonp({"success": "false"});
   }
 });
 
@@ -97,11 +124,9 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+// initDb(function(err){
+//   console.log('Error connecting to Mongo. Message:\n'+err);
+// });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
